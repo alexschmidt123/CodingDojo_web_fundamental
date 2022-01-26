0a74f73efaca689d613f12faf6c7efbb0bba9d17
Gaoming Lin <gaominglins@gmail.com>
2022-01-26 10:37:57 -0500

    publish to github

--- Algorithms/w1d3_Conditionals.js
+++ Algorithms/w1d3_Conditionals.js
@@ -1,12 +1,11 @@
+//Lesson 1
 var isSunny = true;
 var temperature = 45; // let's assume degrees Fahrenheit
 var isRaining = false;
-var whatToBring = "I should bring: ";
-    
+var whatToBring = "I should bring: ";  
 // Conditional statement - starts with the "if" keyword
 // Another way for us to contract the FLOW of ourcode
 // The keyword is followed by a BOOLEAN statement. If the statement resolves to true, then we excute the conditional's CODE BLOCK.
-
 if(isSunny) {
     whatToBring += "sunglasses, ";
 }
@@ -16,6 +15,27 @@ if(temperature < 50) {
 if(isRaining) {
     whatToBring += "an umbrella, ";
 }
-whatToBring += "and a smile!";
-    
+whatToBring += "and a smile!";    
 console.log(whatToBring);
+
+//Lesson 2
+for(var i=10; i>0; i--) {
+    if(i != 2) {
+        console.log(i);
+    } else {
+        console.log("ignition!");
+    }
+}
+console.log("liftoff!");
+
+//Lesson 3
+var countPositives = 0;
+var numbers = [3, 4, -2, 7, 16, -8, 0];   
+// your code here!
+for (var i=0; i<=numbers.length; i++){
+    if (numbers[i]>0){
+        countPositives+=1;
+    }
+}
+// your code here!
+console.log("there are " + countPositives + " positive values");
