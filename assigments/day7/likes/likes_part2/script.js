5c7a5b2d6fe60aaf2d6e1410a5618b105e626f29
Gaoming Lin <gaominglins@gmail.com>
2022-02-02 14:57:39 -0500

    '

---
+++ assigments/day7/likes/likes_part2/script.js
@@ -0,0 +1,20 @@
+var likeCount1 = document.querySelector('#like-count1')
+console.log(likeCount1);
+
+function incrementCount1() {
+    likeCount1.innerText++;
+}
+
+var likeCount2 = document.querySelector('#like-count2')
+console.log(likeCount2);
+
+function incrementCount2() {
+    likeCount2.innerText++;
+}
+
+var likeCount3 = document.querySelector('#like-count3')
+console.log(likeCount3);
+
+function incrementCount3() {
+    likeCount3.innerText++;
+}
